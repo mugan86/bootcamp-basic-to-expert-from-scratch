ae7c5dc2c37d04519c9f35e2b55c5532923540fc
Anartz Mugika Ledo <mugan86@gmail.com>
2021-01-23 16:21:43 +0100

    Asignando de manera automática el tipo de dato con el valor

---
+++ section-3-type-data/1-automatic-assign-type-data.py
@@ -0,0 +1,45 @@
+# Categoría Texto
+# valor de tipo str
+name = "Anartz"
+print(type(name)) # str
+
+# Categoría numérica
+# valor de tipo int
+age = 35
+print(type(age)) # int
+
+# valor de tipo float
+random_number = 35.5
+print(type(random_number)) # float
+
+# valor de tipo complex
+complex_number = 35.5 + 45j
+print(type(complex_number)) # complex
+
+# Categoría secuencial
+# valor de tipo list
+food_list = ["macarrones", "patatas", "filete"]
+print(type(food_list)) # list
+
+# valor de tipo tuple
+food_tuple = ("macarrones", "patatas", "filete")
+print(type(food_tuple)) # tuple
+
+# valor de tipo range
+number_range = range(8)
+print(type(number_range)) # range
+
+# Categoría map
+# valor de tipo dict (diccionario)
+food_dict = {1: "macarrones", 2: "patatas"}
+print(type(food_dict)) # dict
+
+# Conjuntos
+# valor de tipo set
+food_set = {"macarrones", "patatas", "filete"}
+print(type(food_set)) # set
+
+# Booleanos
+# valor de tipo bool
+bool_value = True
+print(type(bool_value)) # bool
