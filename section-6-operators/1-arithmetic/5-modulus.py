6cc1e400d33bae07e2e0ddf34c62c11c9df10238
Anartz Mugika Ledo <mugan86@gmail.com>
2021-01-30 07:09:34 +0100

    Add arithmetic operators exercises

---
+++ section-6-operators/1-arithmetic/5-modulus.py
@@ -0,0 +1,8 @@
+# Modulo (Resto %)
+
+a = 7 % 2 # 3.5 => número entero 3 => 1 (Resultado)
+a = 8 % 2 # 4 => número entero 4 => 0 (Resultado)
+a = 17 % 3 # 5.66 => número entero 5 => 2 (Resultado)
+a = 18 % 4 # 4.5 => número entero 4 => 2 (Resultado)
+
+print("final")
---
+++ section-6-operators/1-arithmetic/7-floor-division.py
@@ -0,0 +1,7 @@
+# Division y redondeo al entero más bajo cercano
+# si tenemos un 2.9 => 2 / 2.34 => 2
+
+a = 18 // 2 # 9 (y no hace redondeo)
+a = 27 / 7 # 3.8... => 3
+a = 27 // 7 # 3.8... => 3
+print("Final")
