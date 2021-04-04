78d649e1bad8448e547ecfa4484b12a0a7aca6fd
Anartz Mugika Ledo <mugan86@gmail.com>
2021-04-04 15:50:31 +0200

    Mejora del código de la parte de excribir, controlando si existe el fichero

--- section-17-files/2-write.py
+++ section-17-files/2-write.py
@@ -3,19 +3,31 @@ import os
 script_directory = os.path.dirname(__file__)
 file_path = f"{script_directory}/2.txt"
 
-f = open(file_path, "w") 
+f = open(file_path, "w")
 # a = Añadir información a la anterior
 # w = sobrescribir toda la información
 input_values = True
 while(input_values):
     input_value = input("Texto a almacenar: ")
     if (input_value != "exit"):
-        f.write("{}\n".format(input_value) )
+        f.write("{}\n".format(input_value))
     else:
         input_values = False
         print("Dejamos de añadir información por escribir 'exit'")
 f.close()
 
 file_path = f"{script_directory}/2-a.txt"
-f = open(file_path, "x") 
+try:
+    # Comprobando si existe el path del fichero
+    if os.path.exists(file_path):  # EXISTE
+        # excepción
+        raise FileExistsError("{} ya existe y no se puede crear"
+                              .format(file_path))
+    else:
+        f = open(file_path, "x")
+        print("{} creado correctamente".format(file_path))
+except FileExistsError as file_exist_error:
+    print("Fichero existente: {}".format(file_exist_error))
+
+
 print("final")
