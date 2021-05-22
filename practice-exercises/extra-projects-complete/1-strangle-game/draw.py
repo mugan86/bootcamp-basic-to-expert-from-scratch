3c0fd79d03e1c280e304bf137bbf769db64680f9
Anartz Mugika Ledo <mugan86@gmail.com>
2021-05-22 11:36:07 +0200

    Ahorcado en desarrollo

---
+++ practice-exercises/extra-projects-complete/1-strangle-game/draw.py
@@ -0,0 +1,121 @@
+from colors import COLORS
+class Draw:
+        def image(self, attemps, hide_word):
+            print("====================")
+            print(f"Intentos: {attemps}")
+            print("====================")
+            if(attemps ==6):
+                print(" ---------------------")
+                for _ in range(16):
+                    print(" |")
+                print("__________")
+            elif(attemps == 5):
+                print(" ---------------------")
+                print(" |                     |")
+                print(" |                     |")
+                print(" |                  -------")
+                print(" |                 | -  -  |")
+                print(" |                 |   o   |")
+                print(" |                  -------")
+                for _ in range(11):
+                    print(" |")
+                print("__________")
+
+            elif(attemps == 4):
+                print(" ---------------------")
+                print(" |                     |")
+                print(" |                     |")
+                print(" |                  -------")
+                print(" |                 | -  -  |")
+                print(" |                 |   o   |")
+                print(" |                  -------")
+                print(" |                     |   ")
+                print(" |                     |   ")
+                print(" |                     |   ")
+                print(" |                     |   ")
+                print(" |                     |   ")
+                for _ in range(6):
+                    print(" |")
+                print("__________")
+
+            elif(attemps == 3):
+                print(" ---------------------")
+                print(" |                     |")
+                print(" |                     |")
+                print(" |                  -------")
+                print(" |                 | -  -  |")
+                print(" |                 |   o   |")
+                print(" |                  -------")
+                print(" |                     |   ")
+                print(" |                   / |   ")
+                print(" |                 /   |   ")
+                print(" |                /    |   ")
+                print(" |                     |   ")
+                for _ in range(6):
+                    print(" |")
+                print("__________")
+                    
+
+            elif(attemps == 2):
+                print(" ---------------------")
+                print(" |                     |")
+                print(" |                     |")
+                print(" |                  -------")
+                print(" |                 | -  -  |")
+                print(" |                 |   o   |")
+                print(" |                  -------")
+                print(" |                     |   ")
+                print(" |                   / | \\ ")
+                print(" |                  /  |   \\ ")
+                print(" |                 /   |     \\ ")
+                print(" |                     |   ")
+                for _ in range(6):
+                    print(" |")
+                print("__________")
+
+            elif(attemps == 1):
+                print(" ---------------------")
+                print(" |                     |")
+                print(" |                     |")
+                print(" |                  -------")
+                print(" |                 | -  -  |")
+                print(" |                 |   o   |")
+                print(" |                  -------")
+                print(" |                     |   ")
+                print(" |                   / | \\ ")
+                print(" |                  /  |   \\ ")
+                print(" |                 /   |     \\ ")
+                print(" |                     |   ")
+                print(" |                    /  ")
+                print(" |                   /      ")
+                print(" |                  /       ")
+                for _ in range(3):
+                    print(" |")
+                print("__________")
+
+            elif(attemps == 0) :
+                print(f"{COLORS['FAIL']}GAME OVER - La palabra oculta es \"{hide_word}\"{COLORS['ENDC']}")
+                print(" ---------------------")
+                print(" |                     |")
+                print(" |                     |")
+                print(" |                  -------")
+                print(" |                 | X  X  |")
+                print(" |                 |   o   |")
+                print(" |                  -------")
+                print(" |                     |   ")
+                print(" |                   / | \\ ")
+                print(" |                  /  |   \\ ")
+                print(" |                 /   |     \\ ")
+                print(" |                     |   ")
+                print(" |                    / \\")
+                print(" |                   /   \\  ")
+                print(" |                  /     \\ ")
+                for _ in range(3):
+                    print(" |")
+                print("__________")
+           
+
+        def hide_word(hideword):
+            print("Palabra a buscar: ")
+            print(hideword)
+
---
+++ practice-exercises/extra-projects-complete/1-strangle-game/game.py
@@ -0,0 +1,32 @@
+class Game:
+        def __init__(self):
+            # Intentos por defecto
+            self.__atemps = 6
+            # Palabra oculta
+            hide_word = '' # usamos 'get_hide_word()'
+            # Convertir string en list de caracteres en minúscula
+            char_list_elements = list(hide_word.lower())
+            # Inicializar lista para los carácteres que vamos introduciendo
+            input_char_list = list()
+            
+            """
+          
+            // Inicializar lista para los carácteres que vamos introduciendo
+            InputCharsList = new List<char>();
+
+            HideWordChars = new List<char>(charListElements);
+
+            CorrectChars = new List<char>(charListElements);
+
+            for(int i = 0; i < HideWordChars.Count; i++) {
+                if (HideWordChars[i] != ' ') {
+                    HideWordChars[i] = '_';
+                    GameWordChardsShow += "_ ";
+                } else {
+                    GameWordChardsShow += "  ";
+                }
+            }
+            Draw.Image(Attemps, HideWord);
+            Draw.HideWord(GameWordChardsShow);
+            """
+        
