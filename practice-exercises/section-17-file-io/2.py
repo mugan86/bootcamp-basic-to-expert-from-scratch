4afa9c36dea99cafea168a76df7741165da673ad
Anartz Mugika <mugan86@gmail.com>
2021-07-02 17:26:59 +0200

    Ejercicio 2 - Ficheros completo

--- practice-exercises/section-17-file-io/2.py
+++ practice-exercises/section-17-file-io/2.py
@@ -1,12 +1,20 @@
 
 import os
 
-def print_n_lines(file_name):
-    script_directory = os.path.dirname(__file__)
-    file_path = f"{script_directory}/{file_name}.txt"
-    txt = open(file_path)
-    number_of_lines = 3
+def print_n_lines(file_name, number_of_lines):
+    
+    try:
+        # Obtenemos la ruta absoluta del directorio en el que estamos trabajando
+        script_directory = os.path.dirname(__file__)
+        file_path = f"{script_directory}/{file_name}.txt"
+        txt = open(file_path)
 
-    for i in range(number_of_lines):
-        line = txt.readline()
-        print(line)
+        for i in range(number_of_lines):
+            line = txt.readline()
+            print(line)
+    except FileNotFoundError:
+        print("No podemos abrir el fichero por no existir")
+    except Exception as except_message:
+        print(except_message)
+
+print_n_lines('exercise2', 3)
