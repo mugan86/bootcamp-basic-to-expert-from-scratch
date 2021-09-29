25bb992da70cd2f0d897a2e5ca1f73ccf9267aa8
Anartz Mugika Ledo <mugan86@gmail.com>
2021-09-29 18:34:29 +0200

    Mockeando y sin mockear un ejemplo

--- section-22-unittesting/03_calculator/test_calculator.py
+++ section-22-unittesting/03_calculator/test_calculator.py
@@ -4,11 +4,23 @@ from calculator import Calculator
 class TestCalculator(TestCase):
     @mock.patch.object(Calculator, "add")
     def test_add(self, mock_response):
-        mock_response.return_value = 230
+        mock_response.return_value = 110
         calculator = Calculator()
         result = calculator.add(12 , 14)
         print(result)
-        self.assertEqual(result, 230)
+        self.assertEqual(result, 110)
+
+    def test_add_not_mock(self):
+        calculator = Calculator()
+        result = calculator.add(12 , 14)
+        print(result)
+        self.assertEqual(result, 26)
+        result = calculator.add(11 , 14)
+        print(result)
+        self.assertEqual(result, 25)
+        result = calculator.add(10 , 14)
+        print(result)
+        self.assertNotEqual(result, 26)
 
 if __name__ == "__main__":
     main()
